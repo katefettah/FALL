71520853aba9232374feccd1a62cf33597021227
katefettah <82536587+katefettah@users.noreply.github.com>
2021-10-16 12:59:33 -0400

    Update MyGitClass.java

--- src/test/java/MyGitClass.java
+++ src/test/java/MyGitClass.java
@@ -1,5 +1,6 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Version biktim artik");
+        System.out.println("Yetti gari from git Hub");
     }
 }
