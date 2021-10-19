ef92d06010a79220acc76a2c627cf2bdb44d3787
katefettah <katefettah@gmail.com>
2021-10-19 13:52:28 -0400

    version 2

--- src/test/java/MyGitClass.java
+++ src/test/java/MyGitClass.java
@@ -1,6 +1,8 @@
 public class MyGitClass {
     public static void main(String[] args) {
         System.out.println("Version biktim artik");
-        System.out.println("Yetti gari from git Hub");
+
+        System.out.println("yetti gari");
+        System.out.println("Ekim 19");
     }
 }
